58305fe9c463026166c7bf840851f29cf9ebe1c5
Yoshihiro Tanaka <contact@cordea.jp>
2019-10-12 15:18:59 +0900

    like entry

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/machinebox/graphql"
 )
 
@@ -20,4 +21,19 @@ func (c *Client) applyHeader(req *graphql.Request) {
 	req.Header.Add("Authorization", "Bearer " + c.Token)
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Accept", "application/json")
+}
+
+func (c *Client) Like(id string) error  {
+	req := graphql.NewRequest(`
+		mutation Like($id: ID!) {
+			like(input: {likableId: $id}) {
+				clientMutationId
+			}
+		}
+	`)
+	req.Var("id", id)
+	c.applyHeader(req)
+
+	ctx := context.Background()
+	return c.client.Run(ctx, req, nil)
 }
