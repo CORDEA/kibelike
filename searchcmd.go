0a46fc73533e81105091bbc01b1acc13cf807e11
Yoshihiro Tanaka <contact@cordea.jp>
2019-10-13 12:36:48 +0900

    implement search cmd

--- searchcmd.go
+++ searchcmd.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/google/subcommands"
 )
 
@@ -22,13 +23,28 @@ func (*SearchCmd) Usage() string {
 	return ""
 }
 
-func (*SearchCmd) SetFlags(f *flag.FlagSet)  {
+func (*SearchCmd) SetFlags(f *flag.FlagSet) {
 }
 
 func (s *SearchCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 	arg := f.Arg(0)
 	if arg == "" {
 		return subcommands.ExitFailure
+	}
+	response, err := s.client.Search(arg)
+	if err != nil {
+		return subcommands.ExitFailure
+	}
+	nodes := response.Search.Nodes
+	for e := range response.Search.Nodes {
+		node := nodes[e]
+		fmt.Println(node.Document.ID)
+		if node.Document.Title != "" {
+			fmt.Println("\t" + node.Document.Title)
+		}
+		if node.Document.Content != "" {
+			fmt.Println("\t" + node.Document.Content)
+		}
 	}
 	return subcommands.ExitSuccess
 }
